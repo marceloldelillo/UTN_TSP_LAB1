0a628c12979f45bdb3a4e8bed7aab5853b0c4e0c
marceloldelillo <55924180+marceloldelillo@users.noreply.github.com>
2020-02-07 16:19:52 -0300

    Inicio
    Incio

---
+++ C++/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+main ()
+{
+
+ cout << endl << " Hola Mundo C++ !! " << endl << endl ;
+
+}
---
+++ C/Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+main ()
+{
+
+ printf("\n Hola Mundo C !! \n\n");
+
+}
