1d57cd456901f936b008789dc9e23b992d5d3879
marceloldelillo <marceloldelillo@hotmail.com>
2020-02-07 16:50:01 -0300

    .

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+
 using namespace std;
 
-main ()
+
+int main ()
 {
 
- cout << endl << " Hola Mundo C++ !! " << endl << endl ;
+    cout << "Hola mundo" << endl;
+    cout << "Hola mundo 4" << endl;
 
+    //cin.ignore();
+    char a;
+
+    a = cin.get();
+
+    cin.ignore();
+    cout << a << endl;
+
+    return 0;
 }
