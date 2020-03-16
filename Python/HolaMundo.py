1acb61f01923f869e3cc76d7d2d64ce540f4f65a
marceloldelillo <marceloldelillo@hotmail.com>
2020-03-16 11:39:27 -0300

    Create HolaMundo.py

---
+++ Python/HolaMundo.py
@@ -0,0 +1,24 @@
+
+
+# Esto es un comentario
+""" 
+    Esto es un bloque de comentarios
+
+"""
+
+a = "Marcelo"
+
+print ("Hola mundo Python")
+print(a)
+
+from tkinter import *
+
+master = Tk(screenName="MLD")
+
+
+Label(text='hola mundo Python').pack()
+Label(text='Es mi primer pantalla').pack()
+
+Button(text='Clic').pack()
+
+mainloop()
